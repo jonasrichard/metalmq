f1c886a1bab0807b77ec1f75d44ce5b0e21fd001
Richard Jonas <richard.jonas.76@gmail.com>
2020-10-26 17:42:32 +0100

    Try to parse method frame

--- client091/src/main.rs
+++ client091/src/main.rs
@@ -6,15 +6,29 @@ struct Client {
     socket: TcpStream,
 }
 
+type Channel = u16;
+
 #[derive(Debug)]
 enum Frame {
-    Method(u16, u16)
+    Method(Channel, MethodClass, Method)
+}
+
+#[derive(Debug)]
+enum MethodClass {
+    Start
+}
+
+#[derive(Debug)]
+enum Method {
+    Todo
 }
 
 async fn process_frames(mut client: Client) -> io::Result<()> {
     let mut buf = [0u8; 1024];
 
-    send_proto_header(&mut client).await;
+    info!("Connected to server");
+
+    send_proto_header(&mut client).await?;
 
     loop {
         match client.socket.read(&mut buf).await {
@@ -43,11 +57,14 @@ fn parse_frame(buf: &[u8]) -> Option<Frame> {
         Some(1) => {
             // check buffer length
             // why we need explicit types here?
-            let channel: u16 = u16::from(buf[1]) << 8 | buf[2] as u16;
-            // next 4 bytes are length
-            let method: u16 = u16::from(buf[7]) << 8 | buf[8] as u16;
+            let channel = parse_u16(&buf, 1);
+            let size = parse_u32(&buf, 3);
+            let end = buf[7 + size as usize];
 
-            Some(Frame::Method(channel, method))
+            info!("Payload size is {}", size);
+            info!("End byte should be 0xCE {:?}", end);
+
+            parse_method_frame(&buf[7..(size + 7) as usize], channel)
         },
         Some(_) =>
             None,
@@ -56,15 +73,61 @@ fn parse_frame(buf: &[u8]) -> Option<Frame> {
     }
 }
 
+fn parse_method_frame(buf: &[u8], channel: u16) -> Option<Frame> {
+    let class_id = parse_u16(&buf, 0);
+    let method_id = parse_u16(&buf, 2);
+    let major_version = buf[4];
+    let minor_version = buf[5];
+
+    let field_table_size = parse_u32(&buf, 6);
+
+    // we don't know the length of the string, so we don't know how
+    // to go on with parsing
+    // TODO use bytes or some buffer library
+    let table_name = parse_simple_string(&buf, 10);
+
+    match class_id {
+        0x0A =>
+            Some(Frame::Method(channel, MethodClass::Start, Method::Todo)),
+        _ =>
+            None
+    }
+}
+
+fn parse_u16(buf: &[u8], i: usize) -> u16 {
+    u16::from(buf[i]) << 8 | u16::from(buf[i+1])
+}
+
+fn parse_u32(buf: &[u8], i: usize) -> u32 {
+    u32::from(buf[i]) << 24 |
+        u32::from(buf[i+1]) << 16 |
+        u32::from(buf[i+2]) << 8 |
+        u32::from(buf[i+3])
+}
+
+fn parse_simple_string(buf: &[u8], i: usize) -> &str {
+    let len = buf[i] as usize;
+
+    std::str::from_utf8(&buf[1..len]).unwrap()
+}
+
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
-    let socket = TcpStream::connect("127.0.0.1:5672").await?;
-
-    process_frames(Client {
-        socket: socket,
-    }).await?;
+    match TcpStream::connect("127.0.0.1:5672").await {
+        Ok(socket) => {
+            if let Err(e) = process_frames(Client {
+                socket: socket,
+            }).await {
+               error!("Comm eror {:?}", e);
+            }
+            ()
+        },
+        Err(e) => {
+            error!("Error {:?}", e)
+        }
+    }
 
     Ok(())
 }
