fb4ef1835f7b71ad82b3c41a55713a2439fe27d4
Richard Jonas <richard.jonas.76@gmail.com>
2020-11-11 11:58:03 +0100

    Get rid of HashMap

--- client091/src/codec.rs
+++ client091/src/codec.rs
@@ -1,5 +1,5 @@
 use bytes::{Buf, BufMut, BytesMut};
-use std::collections::HashMap;
+use log::{info};
 use tokio_util::codec::{Decoder, Encoder};
 
 #[derive(Debug)]
@@ -11,20 +11,22 @@ pub enum AMQPFrame {
 pub struct AMQPCodec {
 }
 
+#[derive(Clone, Debug)]
 pub enum AMQPValue {
     Bool(bool),
     SimpleString(String),
     LongString(String),
-    FieldTable(HashMap<String, AMQPValue>)
+    FieldTable(Vec<(String, AMQPValue)>)
 }
 
+#[derive(Debug)]
 pub struct MethodFrame {
     channel: u16,
     class: u16,
     method: u16,
     version_major: u8,
     version_minor: u8,
-    server_properties: HashMap<String, AMQPValue>,
+    server_properties: Vec<(String, AMQPValue)>,
     mechanisms: String,
     locales: String
 }
@@ -37,7 +39,7 @@ impl Encoder<AMQPFrame> for AMQPCodec {
             AMQPFrame::AMQPHeader =>
                 buf.put(&b"AMQP\x00\x00\x09\x01"[..]),
 
-            AMQPFrame::Method => {
+            AMQPFrame::Method(method_frame) => {
             }
         }
         Ok(())
@@ -54,7 +56,7 @@ impl Decoder for AMQPCodec {
         } else {
             match src.get_u8() {
                 1 => {
-                    let _channel = src.get_u16();
+                    let channel = src.get_u16();
                     // TODO amqp frame can be u32 but Buf handles only usize buffes
                     let frame_len = src.get_u32() as usize;
 
@@ -63,7 +65,7 @@ impl Decoder for AMQPCodec {
                     }
 
                     let mut frame_buf = src.split_to(frame_len);
-                    let frame = decode_method_frame(&mut frame_buf);
+                    let frame = decode_method_frame(&mut frame_buf, channel);
 
                     Ok(Some(frame))
                 },
@@ -75,12 +77,112 @@ impl Decoder for AMQPCodec {
 }
 
 // TODO have an Error type here, and it should be result<>
-fn decode_method_frame(src: &mut BytesMut) -> AMQPFrame {
+fn decode_method_frame(src: &mut BytesMut, channel: u16) -> AMQPFrame {
     let class = src.get_u16();
     let method = src.get_u16();
     let version_major = src.get_u8();
     let version_minor = src.get_u8();
 
+    let arg_len = src.get_u32() as usize;
+    let mut arg_buf = src.split_to(arg_len);
+
+    let args = decode_value_list(&mut arg_buf);
+
+    // TODO find out something more elegant
+
+    let server_properties = match args.get(0) {
+        Some(AMQPValue::FieldTable(hm)) =>
+            Some(hm),
+        _ =>
+            None
+    };
+
+    let mechanism = match args.get(1) {
+        Some(AMQPValue::SimpleString(v)) =>
+            Some(v),
+        _ =>
+            None
+    };
+
+    let locales = match args.get(2) {
+        Some(AMQPValue::SimpleString(v)) =>
+            Some(v),
+        _ =>
+            None
+    };
+
     AMQPFrame::Method(Box::new(MethodFrame {
+        channel: channel,
+        method: method,
+        class: class,
+        version_major: version_major,
+        version_minor: version_minor,
+        server_properties: server_properties.unwrap().to_vec(),
+        mechanisms: mechanism.unwrap().to_owned(),
+        locales: locales.unwrap().to_owned()
     }))
+}
+
+fn decode_value_list(src: &mut BytesMut) -> Vec<AMQPValue> {
+    let mut values = Vec::new();
+
+    while src.has_remaining() {
+        let value = decode_value(src);
+        values.push(value);
+    }
+
+    values
+}
+
+fn decode_value(mut buf: &mut BytesMut) -> AMQPValue {
+    match buf.get_u8() {
+        b't' => {
+            let bool_value = buf.get_u8() != 0;
+
+            AMQPValue::Bool(bool_value)
+        },
+        b'S' => {
+            let string_value = decode_long_string(&mut buf);
+
+            AMQPValue::LongString(string_value)
+        },
+        b'F' => {
+            let table = decode_field_table(&mut buf);
+
+            AMQPValue::FieldTable(table)
+        },
+        t =>
+            panic!("Unknown type {}", t)
+    }
+}
+
+fn decode_short_string(buf: &mut BytesMut) -> String {
+    let len = buf.get_u8() as usize;
+    let sb = buf.split_to(len);
+
+    String::from_utf8(sb.to_vec()).unwrap()
+}
+
+fn decode_long_string(buf: &mut BytesMut) -> String {
+    let len = buf.get_u32() as usize;
+    let sb = buf.split_to(len);
+
+    String::from_utf8(sb.to_vec()).unwrap()
+}
+
+fn decode_field_table(buf: &mut BytesMut) -> Vec<(String, AMQPValue)> {
+    let len = buf.get_u32() as usize;
+    let mut sub_buf = buf.split_to(len);
+    let mut table = Vec::new();
+
+    while sub_buf.has_remaining() {
+        let field_name = decode_short_string(&mut sub_buf);
+        info!("Field name {}", field_name);
+
+        let field_value = decode_value(&mut sub_buf);
+
+        table.push((field_name, field_value));
+    }
+
+    table
 }
--- client091/src/main.rs
+++ client091/src/main.rs
@@ -1,6 +1,6 @@
 mod codec;
 
-use bytes::{BytesMut, Buf, BufMut};
+use bytes::{BytesMut, BufMut};
 use env_logger::Builder;
 use futures::SinkExt;
 use futures::stream::StreamExt;
@@ -70,19 +70,17 @@ enum Value {
     LongString(LongString)
 }
 
-fn handle_frame(mut client: ClientState, frame: Frame) {
+fn handle_frame(client: ClientState, frame: Frame) {
     // match on the frame class and method
     match frame {
-        Frame::MethodFrame(channel, class_method, args) =>
+        Frame::MethodFrame(_channel, class_method, _args) =>
             match class_method {
                 ClassMethod::ConnectionStart =>
                     (),
                     // send start-ok
                 _ =>
                     ()
-            },
-        _ =>
-            ()
+            }
     }
 }
 
@@ -99,63 +97,53 @@ async fn process_frames(mut client: ClientState) -> io::Result<()> {
     Ok(())
 }
 
-async fn process_frames2(mut client: ClientState) -> io::Result<()> {
-    let mut b = [0; 4096];
-    let mut buf = BytesMut::with_capacity(65536);
-
-    info!("Connected to server");
-
-    send_proto_header(&mut client).await?;
-
-    loop {
-        // We should create the fsm of the connection here which
-        // processes the parsed frames
-        match client.socket.read(&mut b).await {
-            Ok(n) if n == 0 =>
-                return Ok(()),
-            Ok(n) => {
-                buf.put(&b[0..n]);
-                let _frame = parse_frame(&mut buf);
-
-                // TODO refactor of course
-                let mut auth = Vec::new();
-                auth.extend_from_slice(b"\x00guest\x00guest");
-
-                let mut capabilities = HashMap::new();
-                capabilities.insert("authentication_failure_on_close".into(), Value::Bool(true));
-                capabilities.insert("basic.nack".into(), Value::Bool(true));
-                capabilities.insert("connection.blocked".into(), Value::Bool(true));
-                capabilities.insert("consumer_cancel_notify".into(), Value::Bool(true));
-                capabilities.insert("publisher_confirms".into(), Value::Bool(true));
-
-                let mut client_properties: HashMap<String, Value> = HashMap::new();
-                client_properties.insert("product".into(), Value::SimpleString("ironmq-client".into()));
-                client_properties.insert("platform".into(), Value::SimpleString("Rust".into()));
-                client_properties.insert("capabilities".into(), Value::FieldTable(capabilities));
-                client_properties.insert("version".into(), Value::SimpleString("0.1.0".into()));
-
-                let args = vec![
-                    Argument { name: "Client-Properties".into(), value: Value::FieldTable(client_properties) },
-                    Argument { name: "Mechanism".into(), value: Value::LongString(String::from_utf8(auth).unwrap()) },
-                    Argument { name: "Response".into(), value: Value::SimpleString("PLAIN".into()) },
-                    Argument { name: "Locale".into(), value: Value::SimpleString("en_US".into()) }
-                ];
-
-                let response = Frame::MethodFrame(0, ClassMethod::ConnectionStartOk, Box::new(args));
-                send_frame(&mut client, response).await?;
-                ()
-            },
-            Err(e) =>
-                error!("Error reading frame {:?}", e)
-        }
-    }
-}
-
-async fn send_proto_header(client: &mut ClientState) -> io::Result<()> {
-    client.socket.write_all(&[b'A', b'M', b'Q', b'P', 0, 0, 9, 1]).await?;
-
-    Ok(())
-}
+//async fn process_frames2(mut client: ClientState) -> io::Result<()> {
+//    let mut b = [0; 4096];
+//    let mut buf = BytesMut::with_capacity(65536);
+//
+//    loop {
+//        // We should create the fsm of the connection here which
+//        // processes the parsed frames
+//        match client.socket.read(&mut b).await {
+//            Ok(n) if n == 0 =>
+//                return Ok(()),
+//            Ok(n) => {
+//                buf.put(&b[0..n]);
+//                let _frame = parse_frame(&mut buf);
+//
+//                // TODO refactor of course
+//                let mut auth = Vec::new();
+//                auth.extend_from_slice(b"\x00guest\x00guest");
+//
+//                let mut capabilities = HashMap::new();
+//                capabilities.insert("authentication_failure_on_close".into(), Value::Bool(true));
+//                capabilities.insert("basic.nack".into(), Value::Bool(true));
+//                capabilities.insert("connection.blocked".into(), Value::Bool(true));
+//                capabilities.insert("consumer_cancel_notify".into(), Value::Bool(true));
+//                capabilities.insert("publisher_confirms".into(), Value::Bool(true));
+//
+//                let mut client_properties: HashMap<String, Value> = HashMap::new();
+//                client_properties.insert("product".into(), Value::SimpleString("ironmq-client".into()));
+//                client_properties.insert("platform".into(), Value::SimpleString("Rust".into()));
+//                client_properties.insert("capabilities".into(), Value::FieldTable(capabilities));
+//                client_properties.insert("version".into(), Value::SimpleString("0.1.0".into()));
+//
+//                let args = vec![
+//                    Argument { name: "Client-Properties".into(), value: Value::FieldTable(client_properties) },
+//                    Argument { name: "Mechanism".into(), value: Value::LongString(String::from_utf8(auth).unwrap()) },
+//                    Argument { name: "Response".into(), value: Value::SimpleString("PLAIN".into()) },
+//                    Argument { name: "Locale".into(), value: Value::SimpleString("en_US".into()) }
+//                ];
+//
+//                let response = Frame::MethodFrame(0, ClassMethod::ConnectionStartOk, Box::new(args));
+//                send_frame(&mut client, response).await?;
+//                ()
+//            },
+//            Err(e) =>
+//                error!("Error reading frame {:?}", e)
+//        }
+//    }
+//}
 
 async fn send_frame(client: &mut ClientState, frame: Frame) -> io::Result<()> {
     match frame {
@@ -189,7 +177,7 @@ async fn send_frame(client: &mut ClientState, frame: Frame) -> io::Result<()> {
     Ok(())
 }
 
-fn write_value(mut buf: &mut BytesMut, value: &Value) {
+fn write_value(buf: &mut BytesMut, value: &Value) {
     match value {
         Value::SimpleString(string) => {
             buf.put_u8(string.len() as u8);
@@ -198,112 +186,6 @@ fn write_value(mut buf: &mut BytesMut, value: &Value) {
         _ =>
             panic!("Unsupported type {:?}", value)
     }
-}
-
-// TODO should be result with an error message
-fn parse_frame(mut buf: &mut BytesMut) -> Option<Frame> {
-    match buf.get_u8() {
-        1 => {
-            // check buffer length
-            // why we need explicit types here?
-            let channel = buf.get_u16();
-            let size = buf.get_u32();
-
-            info!("Payload size is {}", size);
-
-            parse_method_frame(&mut buf, channel)
-        },
-        _ =>
-            None
-    }
-}
-
-fn parse_method_frame(buf: &mut BytesMut, channel: u16) -> Option<Frame> {
-    let class_method = buf.get_u32();
-    let major_version = buf.get_u8();
-    let minor_version = buf.get_u8();
-
-    match class_method {
-        0x000A000A => {
-            let frame_len = buf.get_u32() as usize;
-            let mut sub_buf = buf.split_to(frame_len);
-            let mut props: HashMap<SimpleString, Value> = HashMap::new();
-
-            while sub_buf.has_remaining() {
-                let name = parse_short_string(&mut sub_buf);
-                let value = parse_field_value(&mut sub_buf);
-
-                props.insert(name, value);
-            }
-
-            let frame = ConnectionStart {
-                version_major: major_version,
-                version_minor: minor_version,
-                server_properties: props,
-                mechanisms: "".into(),
-                locales: "".into()
-            };
-
-            // TODO convert struct to vec[args]
-
-            Some(Frame::MethodFrame(channel, ClassMethod::ConnectionStart, Box::new(Vec::new())))
-        },
-        _ =>
-            None
-    }
-}
-
-fn parse_field_value(mut buf: &mut BytesMut) -> Value {
-    match buf.get_u8() {
-        b't' => {
-            let bool_value = buf.get_u8() != 0;
-
-            Value::Bool(bool_value)
-        },
-        b'S' => {
-            let string_value = parse_long_string(&mut buf);
-
-            Value::LongString(string_value)
-        },
-        b'F' => {
-            let table = parse_field_table(&mut buf);
-
-            Value::FieldTable(table)
-        },
-        t =>
-            panic!("Unknown type {}", t)
-    }
-}
-
-fn parse_short_string(buf: &mut BytesMut) -> String {
-    let len = buf.get_u8() as usize;
-    let sb = buf.split_to(len);
-
-    String::from_utf8(sb.to_vec()).unwrap()
-}
-
-fn parse_long_string(buf: &mut BytesMut) -> String {
-    let len = buf.get_u32() as usize;
-    let sb = buf.split_to(len);
-
-    String::from_utf8(sb.to_vec()).unwrap()
-}
-
-fn parse_field_table(buf: &mut BytesMut) -> HashMap<String, Value> {
-    let len = buf.get_u32() as usize;
-    let mut sub_buf = buf.split_to(len);
-    let mut table = HashMap::new();
-
-    while sub_buf.has_remaining() {
-        let field_name = parse_short_string(&mut sub_buf);
-        info!("Field name {}", field_name);
-
-        let field_value = parse_field_value(&mut sub_buf);
-
-        table.insert(field_name, field_value);
-    }
-
-    table
 }
 
 #[tokio::main]
