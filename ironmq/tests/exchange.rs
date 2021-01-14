0a72554134de04ff6c4931e88ebb5a352ddf523a
Richard Jonas <richard.jonas.76@gmail.com>
2021-01-14 19:08:51 +0100

    Tiny test framework

--- ironmq/tests/exchange.rs
+++ ironmq/tests/exchange.rs
@@ -5,6 +5,53 @@ mod helper {
 }
 
 use crate::ironmq_client as client;
+use std::io::Write;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+
+fn write(color: Color, text: &str) {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    stdout.set_color(ColorSpec::new().set_fg(Some(color)));
+
+    writeln!(&mut stdout, "{}", text).unwrap();
+
+    stdout.reset();
+}
+
+struct World {
+    client: Box<dyn client::Client>
+}
+
+impl World {
+    async fn given_a_connection() -> Self {
+        write(Color::Green, "Given a connection");
+
+        let client = client::connect("127.0.0.1:5672").await.unwrap();
+
+        World {
+            client: client
+        }
+    }
+
+    async fn when_it_opens_the_virtual_host(&self) -> &Self {
+        write(Color::Yellow, "When it opens the virtual host");
+
+        self.client.open("/").await.unwrap();
+
+        self
+    }
+
+    async fn then_it_succeeds(&self) {
+        println!("Then is succeeds");
+    }
+}
+
+#[tokio::test]
+async fn first() {
+    World
+        ::given_a_connection().await
+        .when_it_opens_the_virtual_host().await
+        .then_it_succeeds().await;
+}
 
 #[cfg(feature = "integration-tests")]
 #[tokio::test]
