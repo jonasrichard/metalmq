b46af7539b4f904da37e8bc084efbf116dcc6f7a
Richard Jonas <richard.jonas@derivco.se>
2021-05-14 20:52:24 +0200

    Fix publisher consumer test

--- amqp-compliance/test_exchange.py
+++ amqp-compliance/test_exchange.py
@@ -2,6 +2,11 @@ import logging
 import pika
 import pytest
 import threading
+import time
+
+LOG = logging.getLogger()
+queue_bound = threading.Condition()
+message_published = threading.Condition()
 
 def connect_as_guest():
     params = pika.URLParameters('amqp://guest:guest@localhost:5672/%2F')
@@ -11,7 +16,7 @@ def send_message():
     conn = connect_as_guest()
     channel = conn.channel(channel_number=1)
     channel.exchange_declare('my-exchange',
-            exchange_type='direct',
+            exchange_type='topic',
             passive=False,
             durable=False,
             auto_delete=False,
@@ -20,23 +25,38 @@ def send_message():
     channel.queue_declare('my-queue')
     channel.queue_bind('my-queue', 'my-exchange')
 
-    logging.getLogger().info('Start publishing')
+    with queue_bound:
+        queue_bound.notify()
+
+    LOG.info('Publishing message')
     channel.basic_publish('my-exchange',
-            'routing-key',
+            'my-queue',
             'Hey, receiver! How are you?',
             pika.BasicProperties(content_type='text/plain', delivery_mode=1))
 
+    with message_published:
+        message_published.wait()
+
+    channel.queue_unbind('my-queue', 'my-exchange')
+    channel.queue_delete('my-queue')
+    channel.exchange_delete('my-exchange')
+
+    conn.close()
+
 def consume_message():
+    with queue_bound:
+        queue_bound.wait()
     conn = connect_as_guest()
     channel = conn.channel(channel_number=1)
     channel.basic_consume('my-queue', on_message_callback=on_consumer_receive)
-    logging.getLogger().info('start consuming')
     channel.start_consuming()
 
 def on_consumer_receive(channel, method, properties, body):
-    print(body)
-    breakpoint()
+    LOG.info('Got message %s', body)
     channel.stop_consuming()
+
+    with message_published:
+        message_published.notify()
 
 def test_basic_publish(caplog):
     """
@@ -44,10 +64,10 @@ def test_basic_publish(caplog):
     """
     caplog.set_level(logging.INFO)
 
-    receiver = threading.Thread(target=consume_message())
+    receiver = threading.Thread(target=consume_message)
     receiver.start()
 
-    sender = threading.Thread(target=send_message())
+    sender = threading.Thread(target=send_message)
     sender.start()
 
     sender.join()
