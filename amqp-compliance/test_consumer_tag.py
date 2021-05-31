2253dd4ba0ab6b650fbb6c0099ce12461c6cfd8f
jonasrichard <richard.jonas.76@gmail.com>
2021-05-31 17:23:03 +0200

    Exchange tests use test obj

--- amqp-compliance/test_consumer_tag.py
+++ amqp-compliance/test_consumer_tag.py
@@ -48,93 +48,92 @@ class PublishConsumeTest():
     def cancel_consume(self):
         self.channel.stop_consuming()
 
-def on_msg(obj, message_body, expected_body):
-    LOG.info("on_msg %s", message_body)
-    assert message_body.decode("utf-8") == expected_body
-    obj.cancel_consume()
-
 def test_xyz(caplog):
-    body = datetime.datetime.now().strftime("%H:%M:%S.%f")
+    """
+    TODO this will test the redelivery of un-acked messages. We publish messages in
+    an exchange, consume without acking and closing the connection. Reconnect and
+    consuming again, and receive the messages.
+    """
+    expected_body = datetime.datetime.now().strftime("%H:%M:%S.%f")
+
+    def on_message(obj, channel, method, props, body):
+        LOG.info("on_msg %s", body)
+        assert body.decode("utf-8") == expected_body
+
+        obj.cancel_consume()
 
     t = PublishConsumeTest(channel_number=3, exchange_name='ctag-exchange', queue_name='ctag_queue')
     t.declare_exchange()
-    t.publish(body)
-    t.consume(lambda obj, ch, method, props, msg_body: on_msg(obj, msg_body, body))
+    t.publish(expected_body)
+    t.consume(on_message)
     t.close(without_cleanup=True)
 
-current_consumer_tag = ""
-messages_received = 0
-last_delivery_tag = -1
-
-def on_receive(channel, method, properties, body):
-    global messages_received
-    global last_delivery_tag
-
-    LOG.info('Got message %s %s %s', method, properties, body)
-    messages_received += 1
-
-    assert method.consumer_tag == current_consumer_tag
-
-    assert last_delivery_tag < method.delivery_tag
-    last_delivery_tag = method.delivery_tag
-
-    #assert not (method.redelivered)
-
-    assert method.routing_key == 'ctag-queue'
-
-    channel.basic_ack(delivery_tag=method.delivery_tag, multiple=False)
-
-    if messages_received == 10:
-        channel.stop_consuming()
-
-
 def test_server_generated_consumer_tags_one_by_one_ack(caplog):
-    global current_consumer_tag
+    """
+    Publish messages and acking them one by one.
+    """
     exchange_name = 'ctag-exchange'
     queue_name = 'ctag-queue'
 
-    conn = helper.connect()
-    channel = declare_exchange(connection=conn, channel=7, exchange_name=exchange_name, queue_name=queue_name)
+    messages_received = 0
+    last_delivery_tag = -1
+
+    def on_message(obj, channel, method, props, body):
+        nonlocal messages_received
+        nonlocal last_delivery_tag
+
+        LOG.info('Got message %s %s %s', method, props, body)
+        messages_received += 1
+
+        #assert method.consumer_tag == current_consumer_tag
+
+        assert last_delivery_tag < method.delivery_tag
+        last_delivery_tag = method.delivery_tag
+
+        #assert not (method.redelivered)
+
+        assert method.routing_key == 'q-ctag-ack'
+
+        channel.basic_ack(delivery_tag=method.delivery_tag, multiple=False)
+
+        if messages_received == 10:
+            channel.stop_consuming()
+
+    t = PublishConsumeTest(channel_number=7, exchange_name='xchg-ctag-ack', queue_name='q-ctag-ack')
+    t.declare_exchange()
 
     for i in range(0, 10):
-        channel.basic_publish(
-                exchange_name,
-                queue_name,
-                'Message {}'.format(i),
-                pika.BasicProperties(content_type='text/plain', delivery_mode=1))
+        t.publish('Message {}'.format(i))
 
-    current_consumer_tag = channel.basic_consume(queue_name, on_message_callback=on_receive)
-    channel.start_consuming()
-
-    cleanup(channel, exchange_name, queue_name)
-    conn.close()
-
-def on_late_receive(channel, method, properties, body):
-    LOG.info("Late message %s %s %s", method, properties, body)
-
-    channel.basic_ack(delivery_tag=method.delivery_tag, multiple=False)
+    # TODO how to get the consumer-tag of the consuming?
+    t.consume(on_message)
+    t.close()
 
 def test_publish_and_then_consume(caplog):
-    conn = helper.connect()
-    channel = declare_exchange(connection=conn, channel=6, exchange_name='xchg-late-consume', queue_name='q-late-consume')
+    received_messages = 0
+
+    def on_message(obj, channel, method, props, body):
+        nonlocal received_messages
+
+        LOG.info("Late message %s %s %s", method, props, body)
+        channel.basic_ack(delivery_tag=method.delivery_tag, multiple=False)
+        received_messages += 1
+
+        if received_messages == 10:
+            obj.cancel_consume()
+
+    s = PublishConsumeTest(channel_number=6, exchange_name='xchg-late-consume', queue_name='q-late-consume')
+    s.declare_exchange()
 
     for i in range(0, 10):
-        channel.basic_publish(
-                'xchg-late-consume',
-                'q-late-consume',
-                'Message {}'.format(i),
-                pika.BasicProperties(content_type='text/plain', delivery_mode=1))
+        s.publish('Message {}'.format(i))
 
-    conn.close()
+    s.close(without_cleanup=True)
 
-    consumer = helper.connect()
-    channel = consumer.channel(channel_number=6)
+    c = PublishConsumeTest(channel_number=6, exchange_name='xchg-late-consume', queue_name='q-late-consume')
 
-    channel.basic_consume('q-late-consume', on_message_callback=on_late_receive)
-    channel.start_consuming()
-
-    cleanup(channel, 'xchg-late-consume', 'q-late-consume')
-    consumer.close()
+    c.consume(on_message)
+    c.close()
 
 # TODO make a test which sends messages to a channel and consume another (but the same queue)
 # 2021-05-31 11:48:34 [CRITICAL] Received METHOD frame for unregistered channel 6 on <SelectConnection OPEN transport=<pika.adapters.utils.io_services_utils._AsyncPlaintextTransport object at 0x1107c41f0> params=<ConnectionParameters host=localhost port=5672 virtual_host=/ ssl=False>> (connection.py:1592)
