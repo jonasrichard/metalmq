3987282abcfb50220921b88806fdea3e7cd8126e
jonasrichard <richard.jonas.76@gmail.com>
2021-06-07 22:51:16 +0200

    Fixing test

--- amqp-compliance/test_pubsub.py
+++ amqp-compliance/test_pubsub.py
@@ -11,19 +11,17 @@ end = threading.Condition()
 def consumer_thread(channel):
     counter = 0
 
-    def on_message(ch, method, properties, body):
-        LOG.info("on_message %s", method)
-        channel.basic_ack(delivery_tag=method.delivery_tag, multiple=False)
+    for method, props, body in channel.consume("speed1"):
+        channel.basic_ack(method.delivery_tag)
 
         counter += 1
         if counter == NUM:
-            channel.stop_consuming()
+            break
 
-            with end:
-                end.notify()
-
-    channel.basic_consume("speed1", on_message_callback=on_message)
-    channel.start_consuming()
+    channel.cancel()
+    with end:
+        end.notify()
+    channel.close()
 
 def test_one_publisher_one_consumer(caplog):
     publisher = helper.connect()
@@ -38,18 +36,15 @@ def test_one_publisher_one_consumer(caplog):
 
     threading.Thread(target=consumer_thread, args=(channel,)).start()
 
-    time.sleep(0.5)
-
-    LOG.info("Start publishing")
-
     for i in range(0, NUM):
-        channel.basic_publish(
+        pchan.basic_publish(
                 "speed1",
                 "speed1",
                 "Message body {}".format(i),
                 pika.BasicProperties(content_type='text/plain', delivery_mode=1))
 
+    LOG.info("End of publish")
+
     with end:
         end.wait()
-
     LOG.info("End")
