94f01546e730a157a8ef8708168765f70efe2989
jonasrichard <richard.jonas.76@gmail.com>
2021-07-06 22:14:56 +0200

    Check delivered content

--- metalmq/tests/consume.rs
+++ metalmq/tests/consume.rs
@@ -2,10 +2,11 @@ extern crate metalmq_client;
 
 mod helper;
 
+use anyhow::Result;
 use tokio::sync::oneshot;
 
 #[tokio::test]
-async fn consume() -> anyhow::Result<()> {
+async fn consume_one_message() -> Result<()> {
     let exchange = "xchg-consume";
     let queue = "q-consume";
     let mut c = helper::default().connect().await?;
@@ -22,8 +23,34 @@ async fn consume() -> anyhow::Result<()> {
     let msgs = orx.await.unwrap();
     assert_eq!(msgs.len(), 1);
 
+    let msg = msgs.get(0).unwrap();
+    assert_eq!(msg.channel, 1);
+    assert_eq!(msg.consumer_tag, "ctag");
+    assert!(msg.delivery_tag > 0);
+    assert_eq!(msg.length, 5);
+    assert_eq!(msg.body, b"Hello");
+
     ch.close().await?;
     c.close().await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn consume_not_existing_queue() -> Result<()> {
+    let mut c = helper::default().connect().await?;
+    let ch = c.channel_open(2).await?;
+
+    let (otx, _orx) = oneshot::channel();
+
+    let res = helper::consume_messages(&ch, "not-existing-queue", "ctag", otx, 1).await;
+
+    assert!(res.is_err());
+
+    let err = helper::to_client_error(res);
+    assert_eq!(err.channel, Some(2));
+    assert_eq!(err.code, 404);
+    assert_eq!(err.class_method, metalmq_codec::frame::BASIC_CONSUME);
 
     Ok(())
 }
